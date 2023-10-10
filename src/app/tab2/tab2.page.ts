ea00a2ee50c5b7760ae06418b077107b3abb9851
lemgabet <aymenelghoul1993@gmail.com>
2023-10-10 17:52:00 +0100

    hotestnew

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,72 +10,126 @@ import { NgChartsModule } from 'ng2-charts';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  public barChartOptions: any = {
-    responsive: true,
-  };
-  public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
-  public barChartData: any[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  ];
+
+ 
   barChart: any;
+  doughnutChart: any;
+  lineChart: any;
+  barCanvas: any;
+  doughnutCanvas: any;
+  lineCanvas: any;
 
 
 
   constructor() { 
    
   }
-  ionViewDidEnter() {
-    this.createBarChart();
-  }
-  createBarChart() {
-    const element = this.barChart.nativeElement;
-    const ctx = element.getContext('2d');
 
-    const data = {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [
-        {
-          label: 'Example Dataset',
-          data: [12, 19, 3, 5, 2, 3],
+  ngAfterViewInit() {
+    this.barChartMethod();
+    this.doughnutChartMethod();
+    this.lineChartMethod();
+  }
+  
+  barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
             'rgba(255, 206, 86, 0.2)',
             'rgba(75, 192, 192, 0.2)',
             'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
+            'rgba(255,99,132,1)',
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
             'rgba(75, 192, 192, 1)',
             'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
+            'rgba(255, 159, 64, 1)'
           ],
-          borderWidth: 1,
-        },
-      ],
-    };
-
-    const options = {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
+          borderWidth: 1
+        }]
       },
-    };
-
-    const chart = new Chart(ctx, {
-      type: 'bar',
-      data: data,
-      options: options,
+      options: {
+        scales: {
+       
+            ticks: {
+              beginAtZero: true
+            }
+          
+        }
+      }
     });
   }
 
-  
+  doughnutChartMethod() {
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
+      }
+    });
+  }
+
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Sell per week',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
 }
+
+  
+
 
