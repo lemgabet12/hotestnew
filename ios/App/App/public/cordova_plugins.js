ff43356d9b3432b987b96116484582384275f453
lemgabet <aymenelghoul1993@gmail.com>
2023-11-30 15:57:59 +0100

    commit

---
+++ ios/App/App/public/cordova_plugins.js
@@ -0,0 +1,20 @@
+
+  cordova.define('cordova/plugin_list', function(require, exports, module) {
+    module.exports = [
+      {
+          "id": "com.tiltshiftfocus.cordova.plugin.clearCache.Cache",
+          "file": "plugins/com.tiltshiftfocus.cordova.plugin.clearCache/www/Cache.js",
+          "pluginId": "com.tiltshiftfocus.cordova.plugin.clearCache",
+        "clobbers": [
+          "cache"
+        ]
+        }
+    ];
+    module.exports.metadata =
+    // TOP OF METADATA
+    {
+      "com.tiltshiftfocus.cordova.plugin.clearCache": "1.0.5"
+    };
+    // BOTTOM OF METADATA
+    });
+    
